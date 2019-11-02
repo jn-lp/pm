b193bb83be1ae923bf8db6b92608a3408f689de2
Eugene Lepeico <03006jeka@gmail.com>
2019-11-02 12:21:08 +0200

    dev setup

--- electron.js
+++ electron.js
@@ -1,56 +1,56 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow, Menu} = require('electron')
-const path = require('path')
-const url = require('url')
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+const url = require('url');
 
-let mainWindow
+let mainWindow;
 
-function createWindow () {
+function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-		},
-		transparent: false,
-		title: "pm."
-	})
+      preload: path.join(__dirname, 'preload.js'),
+    },
+    transparent: false,
+    title: 'pm.',
+  });
 
-	const menu = Menu.buildFromTemplate([
-		{
-			label: app.name,
-			submenu: [
-				{
-					label:'Exit',
-					click() {
-						app.quit()
-					}
-				}
-			]
-		}
-	])
-	Menu.setApplicationMenu(menu);
+  const menu = Menu.buildFromTemplate([
+    {
+      label: app.name,
+      submenu: [
+        {
+          label: 'Exit',
+          click() {
+            app.quit();
+          },
+        },
+      ],
+    },
+  ]);
+  Menu.setApplicationMenu(menu);
 
-	const startUrl = process.env.ELECTRON_START_URL || "http://localhost:3000" || url.format({
-		pathname: path.join(__dirname, '/../build/index.html'),
-		protocol: 'file:',
-		slashes: true
-	});
+  const startUrl = process.env.ELECTRON_START_URL || 'http://localhost:3000' || url.format({
+    pathname: path.join(__dirname, '/../build/index.html'),
+    protocol: 'file:',
+    slashes: true,
+  });
 
-	mainWindow.loadURL(startUrl);
+  mainWindow.loadURL(startUrl);
   mainWindow.on('closed', () => {
-		mainWindow = null
-  })
+    mainWindow = null;
+  });
 }
 
 app.on('ready', () => {
-	createWindow()
-})
+  createWindow();
+});
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
+  if (process.platform !== 'darwin') app.quit();
+});
 
 app.on('activate', () => {
-  if (mainWindow === null) createWindow()
-})
+  if (mainWindow === null) createWindow();
+});
